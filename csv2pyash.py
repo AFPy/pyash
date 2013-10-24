ab6c343311db1adc5bfb7d0dbe68f2ee56236642
Gael Pasgrimaud <gael@gawel.org>
2013-10-25 01:09:12 +0200

    real improvments

--- csv2pyash.py
+++ csv2pyash.py
@@ -4,23 +4,43 @@
 import csv
 from datetime import datetime
 
+from chut import console_script
 from jinja2 import Template
 
 
-TEMPLATE = u'''{{ date.strftime('%Y/%m/%d') }} {{ amount }}€ {{ type }}
-    {{ description }}
+TEMPLATE = u'''
+{{ r.date }} {{ r.amount }}€ {{r.kind}} {{ r.type }}{{ r.status }}
+    {{ r.description }}
+    {{ r.comment or '' }}
 
 '''
+
+types = {
+    'Teeshirt': 'Goodies',
+    'FraisPubli': 'Goodies',
+    'Alimentation': 'Alimentation',
+    'Transport': 'Transport',
+    'Partenaire:PyConFR08': 'Pycon08',
+    'Partenaire:PyConFR07': 'PyCon07',
+    'SCBC': 'VariousEvents',
+    'Fraisbanca': 'FraisBanque',
+    u'Hébergement': 'Hosting',
+    'Fraish': u'Hebergement',
+    'Fraisdeser': u'FraisDivers',
+    'VariousEvents': 'SponsoringEvents',
+    'SponsoringEvenements': 'SponsoringEvents',
+}
 
 
 def transform_item(record):
     record = {key.lower(): value for key, value in record.items()}
-    record['date'] = datetime.strptime(record['date'], '%d/%m/%y').date()
+    record['date'] = datetime.strptime(record['date'],
+                                       '%d/%m/%y').strftime('%Y/%m/%d')
     record['amount'] = float(record['amount'].replace(',', '.'))
-    record['comment'] = record['comment'] or None
-    record['description'] = record['description'] or None
-    record['type'] = record['type'] or None
-    record['info'] = record['info'] or None
+    record['comment'] = record['comment'].decode('utf8') or None
+    record['description'] = record['description'].decode('utf8') or None
+    record['type'] = record['type'].decode('utf8').replace(' ', '')
+    record['info'] = record['info'].decode('utf8') or None
 
     if record['info'] is not None:
         if record['comment'] is None:
@@ -28,21 +48,74 @@ def transform_item(record):
         else:
             record['comment'] += u' ; ' + record['info']
 
-    for key in ['conciled date', 'ending date', 'frequency', 'status', 'kind',
-                'taxes', 'info']:
-        del record[key]
+    if record['amount'] == 0:
+        record['type'] = 'Cotisation'
+        record['amount'] = 20.
+
+    st = record['status']
+    if st.startswith('Pending'):
+        record['status'] = u' P'
+    else:
+        record['status'] = u' X'
+
+    t = record['type']
+    for k, v in types.items():
+        if t.startswith(k):
+            t = v
+    record['type'] = t
+    if not t:
+        if record['description'] == 'Sac Ecobags':
+            t = 'Goodies'
+        elif record['amount'] == 20:
+            t = 'Cotisation'
+        else:
+            raise LookupError(t)
+        record['type'] = t
+
+    k = record['kind']
+    if k.startswith('TransferTransaction'):
+        k = 'Transfer'
+    elif k.startswith('ServiceCharge'):
+        k = 'ServiceCharge'
+    elif k.startswith('StandingOrder'):
+        k = 'Order'
+    elif k.startswith('Check'):
+        k = 'Check'
+    elif k.startswith('CreditCard'):
+        k = 'CreditCard'
+    elif k.startswith('Cash'):
+        k = 'Cash'
+    elif k.startswith('Other'):
+        if t == 'Don':
+            k = 'Transfer'
+        elif record['description'] == 'Sac Ecobags':
+            record['type'] = 'Goodies'
+            k = 'Check'
+        elif 'shirt' in record['description']:
+            t = 'Goodies'
+            k = 'Check'
+        else:
+            k = 'Other'
+            if record['amount'] != 20:
+                if 'pycon' in record['description'].lower():
+                    k = 'Check'
+                elif record['amount'] in (100, 200, 400):
+                    k = 'Check'
+    else:
+        raise LookupError(k)
+    record['kind'] = k
+
+    for key in ['conciled date', 'ending date', 'frequency', 'taxes']:
+        record[key] = record[key].decode('utf8')
     return record
 
 
 def print_record(record):
     template = Template(TEMPLATE)
     move = template.render(
-        date=record['date'],
-        description=record['description'],
-        type=record['type'].replace(' : ', ':'),
-        amount=record['amount']
-    )
-    print(move)
+        r=record,
+    ).encode('utf8')
+    print(move.strip() + '\n')
 
 
 def parse_csv(filename):
@@ -52,8 +125,12 @@ def parse_csv(filename):
             yield transform_item(record)
 
 
-if __name__ == '__main__':
-    import sys
-    records = parse_csv(sys.argv[1])
+@console_script
+def csv2pyash(args):
+    """Usage: %prog <csv>"""
+    records = parse_csv(args['<csv>'])
     for record in records:
         print_record(record)
+
+if __name__ == '__main__':
+    csv2pyash()
--- pyash.py
+++ pyash.py
@@ -1,84 +1,188 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
+from jinja2 import Template
+from chut import console_script
+from decimal import Decimal
 import datetime
 
-from decimal import Decimal
+DATE_FORMAT = '%Y/%m/%d'
+
+table = Template('''
+{%- for label, amount in values %}
+{{ '+-{0:20s}+{1:11s}-+'.format('-' * 20, '-' * 11) }}
+{{ '| {0:20s}|{1:11.2f} |'.format(label, amount) }}
+{%- endfor %}
+{{ '+-{0:20s}+{1:11s}-+'.format('-' * 20, '-' * 11) }}
+''')
+
+
+def render_table(values):
+    return table.render(values=values) + '\n'
+
+
+class Balance(dict):
+
+    def __init__(self, reverse=False):
+        self.reverse = reverse
+        self.coef = reverse and 1 or -1
+        self.title = reverse and u'Recettes' or u'Depenses'
+
+    def __iter__(self):
+        items = sorted([(v * self.coef, k) for k, v in self.items()],
+                       reverse=True)
+        return iter([(k, v) for v, k in items])
+
+    def sum(self):
+        return sum(self.values()) * self.coef
+
+    def __str__(self):
+        title = u'%s\n%s\n' % (self.title, u'=' * len(self.title))
+        values = list(self) + [('Total', self.sum())]
+        return title + render_table(values)
+
+
+class Move(dict):
+
+    template = Template(u'''
+{{ m.date }} {{ m.amount }}€ {{m.kind}} {{ m.category }} {{ m.status }}
+    {{ m.description }}
+    {{ m.comment or '' }}
+''')
+
+    def __init__(self, line):
+        date_str, amount_str, kind, category, status = line.strip().split(' ')
+        date = datetime.datetime.strptime(date_str, DATE_FORMAT)
+        amount = Decimal(amount_str.rstrip(u'\u20ac'))
+        self.update({
+            'date': date,
+            'amount': amount,
+            'category': category,
+            'status': status,
+            'kind': kind,
+            'description': None,
+            'comment': '',
+        })
+
+    def add(self, line):
+        if line.strip():
+            if self['description'] is None:
+                self['description'] = line.strip()
+            else:
+                self['comment'] += line.strip(' ')
+
+    def __cmp__(self, other):
+        return cmp(self['date'], other['date'])
+
+    def __str__(self):
+        m = dict(self.copy())
+        m['date'] = self['date'].strftime(DATE_FORMAT)
+        return self.template.render(m=m).encode('utf8').lstrip()
 
 
 class MovesFile:
 
-    def __init__(self, filename):
-        assert filename, 'A filename must be given'
-        self.file = open(filename, 'r')
-
-    def parse(self):
-        move = None
-        for line in self.file:
-            if move and (line in ['', '\n'] or line[:4] != '    '):
-                yield move
-                move = None
-            if line == '':
-                break
-            if line == '\n':
-                continue
-            if move and line[:4] == '    ':
-                    move['description'] += line[4:]
-            if not move:
-                move = self.parse_1st_line(line)
-        self.file.close()
-
-    def parse_1st_line(self, line):
-        date_str, amount_str, category = line[:-1].split(' ', 2)
-        date = datetime.datetime.strptime(date_str, '%Y/%m/%d')
-        amount = Decimal(amount_str[:-1])
-        return {
-            'date': date,
-            'amount': amount,
-            'category': category,
-            'description': '',
+    def __init__(self, args):
+        self.args = args
+        self.file = open(args['-i'], 'r')
+        self.start_date = datetime.datetime.strptime(args['-s'], DATE_FORMAT)
+        if args['-e'] == 'Now':
+            self.end_date = datetime.datetime.now()
+        else:
+            self.end_date = datetime.datetime.strptime(args['-e'], DATE_FORMAT)
+        self.moves = sorted(self.parse())
+        self.balances = {
+            'in': Balance(reverse=True),
+            'out': Balance(),
         }
-
-    def balance(self):
-        balance_in = {}
-        balance_out = {}
-        for move in self.parse():
+        for move in self.moves:
             if move['amount'] > 0:
-                balance = balance_in
+                balance = self.balances['in']
             else:
-                balance = balance_out
+                balance = self.balances['out']
 
             category = move['category']
             if category in balance:
                 balance[category] += move['amount']
             else:
                 balance[category] = move['amount']
-        return balance_in, balance_out
+
+    def title(self):
+        title = 'Période du %s au %s\n' % (
+            self.start_date.strftime(DATE_FORMAT),
+            self.end_date.strftime(DATE_FORMAT)
+        )
+        sep = '=' * len(title)
+        sep += '\n'
+        return sep + title + sep
+
+    def balance(self):
+        i = self.balances['in']
+        o = self.balances['out']
+        values = [
+            (i.title, i.sum()),
+            (o.title, o.sum()),
+            ('Resultat', i.sum() - o.sum()),
+        ]
+        return render_table(values=values)
+
+    def iterator(self):
+        with open(self.args['-i']) as fd:
+            for line in fd:
+                yield line.decode('utf8')
+
+    def parse(self):
+        move = None
+        line = True
+        fd = self.iterator()
+        line = fd.next()
+        while True:
+            if line[:4].isdigit():
+                move = Move(line)
+                line = fd.next()
+                while not line[:4].isdigit():
+                    move.add(line)
+                    try:
+                        line = fd.next()
+                    except StopIteration:
+                        break
+                if self.filter(move):
+                    yield move
+            else:
+                line = fd.next()
+
+    def filter(self, move):
+        if self.args['-g'] and self.args['-g'] not in str(move).lower():
+            return
+        if self.args['-p'] and not move['status'].upper() == 'P':
+            return
+        if self.args['-x'] and not move['status'].upper() == 'X':
+            return
+        if self.start_date <= move['date'] <= self.end_date:
+            return True
 
 
-TEMPLATE = u'''Revenus :
-{%- for category, amount in balance_in.items() %}
-     {{ category }} : {{ amount }}
-{%- endfor %}
-     =============================
-     Total : {{ balance_in.values()|sum }}
+@console_script
+def pyash(args):
+    """
+    Usage: %prog [options] balance
+           %prog [options] show
 
-Dépenses :
-{%- for category, amount in balance_out.items() %}
-     {{ category }} : {{ amount }}
-{%- endfor %}
-     =============================
-     Total : {{ balance_out.values()|sum }}
-
-'''
-
-
-def print_balance(balance_in, balance_out):
-    from jinja2 import Template
-    template = Template(TEMPLATE)
-    print(template.render(balance_in=balance_in, balance_out=balance_out))
+    -i FILENAME     Input file [Default: afpy.ash]
+    -s DATE         Start date [Default: 2000/01/01]
+    -e DATE         Start date [Default: Now]
+    -p              Show pendings
+    -x              Show checked
+    -g PATTERN      Grep
+    """
+    moves = MovesFile(args)
+    if args['balance']:
+        print(moves.title())
+        print(moves.balance())
+        print(moves.balances['in'])
+        print(moves.balances['out'])
+    elif args['show']:
+        for m in moves.moves:
+            print(m)
 
 if __name__ == '__main__':
-    import sys
-    moves = MovesFile(sys.argv[1])
-    balin, balout = moves.balance()
-    print_balance(balin, balout)
+    pyash()
